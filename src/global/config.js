84415709fb6c50ce8fc50b81b2279c8572c81278
amir_abbasy <87001985+amir-abbasy@users.noreply.github.com>
2022-07-08 12:46:05 +0530

    fix api url err

--- src/global/config.js
+++ src/global/config.js
@@ -1,8 +1,9 @@
 var Config = {
   dev: false,
-  api_url: (() => this.dev)
+  api_url: 'https://intra-arb-bot-test.herokuapp.com',
+  api_url_: (() => this.dev)
     ? 'http://127.0.0.1:7300'
-    : 'https://intra-arb-bot-test.herokuapp.com/',
+    : 'https://intra-arb-bot-test.herokuapp.com',
 }
 
 
